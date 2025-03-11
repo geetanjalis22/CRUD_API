b80c72c932a2129f8ac21d5c171e580d9ce937f2
Geetanjali <72863114+geetanjalis22@users.noreply.github.com>
2025-03-11 22:44:38 +0530

    Delete src directory

--- src/routes/articleRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createArticle, getArticles } from "../controllers/articleController";
-
-const router = express.Router();
-
-router.post("/", createArticle);
-router.get("/", getArticles);
-
-export default router;
--- src/routes/tagRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createTag, getTags } from "../controllers/tagController";
-
-const router = express.Router();
-
-router.post("/", createTag);
-router.get("/", getTags);
-
-export default router;
