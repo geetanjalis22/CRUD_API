9498ecd46884efeeab10911928aeacdec8370da5
Geetanjali <72863114+geetanjalis22@users.noreply.github.com>
2025-03-18 19:39:01 +0530

    Delete src directory

--- src/models/Category.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const CategorySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  description: { type: String },
-});
-
-export default mongoose.model("Category", CategorySchema, "categories");
--- src/models/Tag.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const TagSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  description: { type: String },
-});
-
-export default mongoose.model("Tag", TagSchema);
