bf8664ca05dbce5b9a2376c3945f4c0efeab341f
Geetanjali <72863114+geetanjalis22@users.noreply.github.com>
2025-03-20 17:39:28 +0530

    Delete src directory

--- src/controllers/authController.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-
-export const logout = async (req: Request, res: Response) => {
-    // Although JWTs cannot be invalidated server-side without Redis or a database,
-    // The client should discard the token to effectively log out.
-    res.json({ message: 'Logout successful' });
-};
